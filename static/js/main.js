cdd7ee9346d34eba594020a73dbc999576c22e53
Daniel F <danielfranco2011@gmail.com>
2025-05-11 15:14:10 -0700

    tidy main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,40 +1,5 @@
 
-/// Queue Page vars /////
-var activeCourtStartTimes = document.querySelectorAll("#active-courts>.group>.start-time>.time")
-/// Main Page vars /////
-const playerSection = document.querySelector("#players-section")
-let playerSectionCount = 1
-const addPlayerButton = document.querySelector("#add-player")
-
-if (addPlayerButton){
-    addPlayerButton.addEventListener("click", ()=>{
-    
-        const newPlayerInfo = document.createElement("div")
-        const newNameLabel = document.createElement("label") 
-        const newNameInput = document.createElement("input")
-
-        newPlayerInfo.classList.add("player-info")
-
-        newNameLabel.setAttribute("for", "name-"+playerSectionCount)
-        newNameLabel.innerText = "Name: "
-
-        newNameInput.setAttribute("type", "text")
-        newNameInput.setAttribute("id", "name"+playerSectionCount)
-        newNameInput.setAttribute("name", "name"+playerSectionCount)
-
-        playerSectionCount += 1
-
-        newPlayerInfo.appendChild(newNameLabel)
-        newPlayerInfo.appendChild(newNameInput)
-        playerSection.insertBefore(newPlayerInfo, addPlayerButton)
-
-        if (playerSectionCount >= 4){
-                addPlayerButton.disabled = true
-        }
-    })
-}
-
-console.log(activeCourtStartTimes)
+// Helper Function //
 function parseStartTime(startTime){
     let date_time_offset_tz = startTime.split(" ")
     date_time_offset_tz[1] = date_time_offset_tz[1].substring(0, 12)
@@ -58,21 +23,72 @@ function formatElapsedTime(elapsedTime){
     }
     return min+" "+sec
 }
-activeCourtStartTimes.forEach((startTime)=>{
-    console.log(parseStartTime(startTime.innerHTML))
-    let time = new Date(parseStartTime(startTime.innerHTML))
-    console.log("Hello World")
-    console.log(time)
-    let currentTime = new Date()
-    console.log(currentTime)
-    let timeElapsed = currentTime - time // in milliseconds
-    timeElapsed /= 1000
-    startTime.innerHTML = Math.floor(timeElapsed/60)+"m "+Math.floor(timeElapsed%60)+"s"
 
-});
 
-setInterval(()=>{
+// Page Setup //
+function setupSignUp(){
+    const playerSection = document.querySelector("#players-section")
+    let playerSectionCount = 1
+    const addPlayerButton = document.querySelector("#add-player")
+    
+    function onAddPlayer(){
+        const newPlayerInfo = document.createElement("div")
+        const newNameLabel = document.createElement("label") 
+        const newNameInput = document.createElement("input")
+
+        newPlayerInfo.classList.add("player-info")
+
+        newNameLabel.setAttribute("for", "name-"+playerSectionCount)
+        newNameLabel.innerText = "Name: "
+
+        newNameInput.setAttribute("type", "text")
+        newNameInput.setAttribute("id", "name"+playerSectionCount)
+        newNameInput.setAttribute("name", "name"+playerSectionCount)
+
+        playerSectionCount += 1
+
+        newPlayerInfo.appendChild(newNameLabel)
+        newPlayerInfo.appendChild(newNameInput)
+        playerSection.insertBefore(newPlayerInfo, addPlayerButton)
+
+        if (playerSectionCount >= 4){
+                addPlayerButton.disabled = true
+        }
+    }
+
+    addPlayerButton.addEventListener("click", onAddPlayer)
+}  
+
+function setupQueue(){
+
+    var activeCourtStartTimes = document.querySelectorAll("#active-courts>.group>.start-time>.time")
+    
     activeCourtStartTimes.forEach((startTime)=>{
-        startTime.innerHTML = formatElapsedTime(startTime.innerHTML)
-    })
-}, 1000)
+        console.log(parseStartTime(startTime.innerHTML))
+        let time = new Date(parseStartTime(startTime.innerHTML))
+        console.log("Hello World")
+        console.log(time)
+        let currentTime = new Date()
+        console.log(currentTime)
+        let timeElapsed = currentTime - time // in milliseconds
+        timeElapsed /= 1000
+        startTime.innerHTML = Math.floor(timeElapsed/60)+"m "+Math.floor(timeElapsed%60)+"s"
+    
+    });
+    
+    setInterval(()=>{
+        activeCourtStartTimes.forEach((startTime)=>{
+            startTime.innerHTML = formatElapsedTime(startTime.innerHTML)
+        })
+    }, 1000)
+}
+
+var path = window.location.pathname
+switch(path){
+    case "/":
+        setupSignUp()
+        break
+    case "/queue":
+        setupQueue()
+        break
+}
